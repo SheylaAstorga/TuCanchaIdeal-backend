f68f8fef14891afb9aae8b2a9a38c18717f17cdb
sheyla astorga <sheylaastorga1998@gmail.com>
2024-05-01 19:49:35 -0300

    agregue un middleware para cargar el index como archivo estatico

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import morgan from 'morgan';
+import {fileURLToPath} from 'url';
+import path from 'path';
 
 const app = express();
 app.set('port',process.env.PORT ||4000);
@@ -13,3 +15,14 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+app.use(express.static((path.join(__dirname,'/public'))))
+
+
+
+app.get('/', (req,res)=>{
+    console.log('Alguien solicito algo')
+    res.send('respuesta desde nuestro backend de tu cancha ideal')
+})
