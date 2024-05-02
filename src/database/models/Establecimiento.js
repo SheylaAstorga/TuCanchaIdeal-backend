70a2e800ce3641886931430f335bfa42c916c762
K Stephan Torres <kevinstephantorres@gmail.com>
2024-05-02 18:10:17 -0300

    Se creo el Schema de Estableciomiento

---
+++ src/database/models/Establecimiento.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import Cancha from "./Cancha.js";
+
+const establecimientoSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 100,
+    },
+    estado: {
+        type: String,
+        enum: ["abierto", "cerrado"],
+        default: "cerrado",
+    },
+    canchas: [
+        {
+            cancha: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: Cancha,
+                required: true,
+            },
+        },
+    ],
+    lat: {
+        type: Number,
+        required: true,
+    },
+    lng: {
+        type: Number,
+        required: true,
+    },
+    calle: {
+        type: String,
+        required: true,
+    },
+    telefono: {
+        type: Number,
+        required: true,
+    },
+    fotos: [{
+        foto: {
+            type: String,
+            required: true,
+        }
+    }],
+
+})
+
+const Establecimiento = mongoose.model('establecimiento', establecimientoSchema)
+
+export default Establecimiento
