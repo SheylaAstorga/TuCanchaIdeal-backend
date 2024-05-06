3898f40cb16a090a0af12e7b59feba859bf62885
Nano <luxorius07@gmail.com>
2024-05-06 12:42:19 -0300

    se ha realizado el read de contabilidad

--- src/routes/canchas.routes.js
+++ src/routes/canchas.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { listarCanchas } from "../controllers/canchas.controllers.js";
+import { obtenerContabilidad } from "../controllers/contabilidad.controllers.js";
 
 const router = Router();
 
-router.route('/canchas').get(listarCanchas)
+router.route("/canchas").get(listarCanchas);
+router.route("/contabilidad").get(obtenerContabilidad);
 
 export default router;
