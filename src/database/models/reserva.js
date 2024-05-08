ba1dc6634b31ab4706800a231bc6df843c6438d4
Elias Juarez <juarezelias83@gmail.com>
2024-05-08 03:11:58 -0300

    se creo el archivo reserva.js para el Schema de la reserva

--- src/database/models/reserva.js
+++ src/database/models/reserva.js
@@ -1,24 +1,7 @@
-const { Schema } = require("mongoose");
+import mongoose, {Schema} from "mongoose"
 
 
 const reservaSchema = new Schema({
-    nombreCompleto: {
-        type: String,
-        required: true,
-        unique: false,
-        minLength: 3,
-        maxLength: 30
-      },
-    telefono:{
-        type: Number,
-        required: true,
-        min: 10, 
-        max: 16
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-      }
+
 
 })
