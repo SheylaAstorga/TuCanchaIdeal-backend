1cbe2b0e47825094bee1a49aae52111e536fc0b0
Elias Juarez <juarezelias83@gmail.com>
2024-05-08 03:18:54 -0300

    se agrego el campo canchas dentro del esquema de la reserva

--- src/database/models/reserva.js
+++ src/database/models/reserva.js
@@ -1,7 +1,14 @@
-import mongoose, {Schema} from "mongoose"
-
+import mongoose, { Schema } from "mongoose";
+import Cancha from "./Cancha.js";
 
 const reservaSchema = new Schema({
-
-
-})
+  canchas: [
+    {
+      cancha: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Cancha,
+        required: true,
+      },
+    },
+  ],
+});
