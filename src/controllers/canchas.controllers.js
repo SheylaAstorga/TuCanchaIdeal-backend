d7b89e0a60746ee0720f98e89f76346f557af679
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-05-10 10:17:48 -0300

    se creó la logica en el controlador para actualizar y también su ruta

--- src/controllers/canchas.controllers.js
+++ src/controllers/canchas.controllers.js
@@ -12,3 +12,20 @@ export const obtenerCancha = async (req, res) => {
     res.status(404).json({ mensaje: "Cancha No Encontrada" });
   }
 };
+
+export const actualizarCancha = async (req, res) => {
+  try {
+    const cancha = Cancha.findById(req.params.id);
+    if (!cancha) {
+      res.status(404).json({ mensaje: "La Cancha no Existe" });
+    }
+
+    await Cancha.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ mensaje: "La Cancha fue modificada exitosamente!" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ mensaje: "Error al actualizar los datos de la cancha" });
+  }
+};
--- src/routes/canchas.routes.js
+++ src/routes/canchas.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { listarCanchas, obtenerCancha } from "../controllers/canchas.controllers.js";
+import {
+  actualizarCancha,
+  listarCanchas,
+  obtenerCancha,
+} from "../controllers/canchas.controllers.js";
 
 const router = Router();
 
-router.route('/canchas').get(listarCanchas)
-router.route('/canchas/:id').get(obtenerCancha)
+router.route("/canchas").get(listarCanchas);
+router.route("/canchas/:id").get(obtenerCancha).put(actualizarCancha);
 
 export default router;
