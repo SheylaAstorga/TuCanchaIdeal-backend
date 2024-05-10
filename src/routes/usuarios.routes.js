e3b4dddda0f4e05eec1ab624d5d2e6b2c29958d7
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-05-10 09:56:25 -0300

    pequeña modificación en el modelo de usuario, se creó el controlador para eliminar un usuario y se creó su ruta

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { listarUsuarios } from "../controllers/usuarios.controllers.js";
+import { eliminarUsuarios, listarUsuarios } from "../controllers/usuarios.controllers.js";
 
 
 const router = Router();
 
 router.route('/usuarios').get(listarUsuarios)
+router.route('/usuarios/:id').delete(eliminarUsuarios)
 
 export default router;
