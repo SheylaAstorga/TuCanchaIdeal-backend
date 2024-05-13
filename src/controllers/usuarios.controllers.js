451ab9a28c6956daf4cf50db4edb244f24f5f13a
K Stephan Torres <kevinstephantorres@gmail.com>
2024-05-13 16:13:50 -0300

    Se importo el modelo de Usuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,4 +1,8 @@
+import Usuario from "../database/models/usuario.js"
+
+
 export const listarUsuarios =(req,res)=>{
     console.log('Desde listar usuarios');
     res.send('Enviar lista de usuarios')
 }
+
