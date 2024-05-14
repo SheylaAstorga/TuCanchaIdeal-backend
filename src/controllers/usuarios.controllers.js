b7bfef0d9dfcf53a89cf2148379159aa36964d73
K Stephan Torres <kevinstephantorres@gmail.com>
2024-05-14 16:20:35 -0300

    Se creo la funcionalidad editarUsuario en usuarios.controllers

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,5 +1,18 @@
 import Usuario from "../database/models/usuario.js"
 
+export const editarUsuario = async( req, res )=>{
+try {
+    const busquedaResultado = await Usuario.findById(req.params.id)
+    if(!busquedaResultado){
+        return res.status(404).json({mensaje: "No se pudo editar el usuario, el id es incorrecto"})
+    }
+    await Usuario.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).json({mensaje: "El usuario fue modificado exitosamente."})
+} catch (error) {
+    console.log("🚀 ~ editarUsuario ~ error:", error)
+    res.status(500).json({mensaje: "Ocurrio un error al intentar editar "})
+}
+}
 
 export const listarUsuarios =(req,res)=>{
     console.log('Desde listar usuarios');
