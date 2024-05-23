e621210cc74b54b454e918f67abffa2d9e948f4f
Elias Juarez <juarezelias83@gmail.com>
2024-05-23 16:35:37 -0300

    se creo la rama fixModeloReserva y se modifico el campo de canchas en el Schema

--- src/database/models/reserva.js
+++ src/database/models/reserva.js
@@ -1,16 +1,10 @@
 import mongoose, { Schema } from "mongoose";
-import Cancha from "./Cancha.js";
 
 const reservaSchema = new Schema({
-  canchas: [
-    {
-      cancha: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: Cancha,
-        required: true,
-      },
-    },
-  ],
+  canchas: {
+    type: Number,
+    required: true,
+  },
   fecha: {
     type: Date,
     required: true,
