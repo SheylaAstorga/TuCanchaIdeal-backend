d1688d69cdc6fcc61a2cfddfd23da8003a60833d
sheyla astorga <sheylaastorga1998@gmail.com>
2024-05-23 11:38:20 -0300

    se creo la funcion para hacer el read por ID de las reservas de las canchas , en el controllador de reservas y se agrego la ruta correspondiente

--- src/routes/reserva.routes.js
+++ src/routes/reserva.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { obtenerReserva } from "../controllers/reserva.controllers.js";
 
 const router = Router()
 
-router.route('/reserva').post(crearReserva)
-
+router.route('/reserva').post(crearReserva);
+router.route('/reserva/:id').get(obtenerReserva);
 export default router;
